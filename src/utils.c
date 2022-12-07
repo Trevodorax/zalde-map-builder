f61a9868d3769e123d216411e0909510011e87ca
Trevodorax <phvcgdx@gmail.com>
2022-12-07 18:43:58 +0100

    5: add tileset and fileExists function

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,11 @@
 #include "utils.h"
+
+int fileExists(const char * fileName) {
+    FILE * file = fopen(fileName, "r");
+    if(file == NULL) {
+        fclose(file)
+        return 0;
+    }
+    fclose(file);
+    return 1;
+}
